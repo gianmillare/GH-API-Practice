ab8da698a4baec93a33bcafabefc2e27a8c1f521
gianmillare <gianmillaregsm@gmail.com>
2020-03-22 14:40:17 -0700

    returned specific information for the first repository

--- python_repos.py
+++ python_repos.py
@@ -23,18 +23,43 @@ headers = {'Accept': 'application/vnd.github.v3+json'} # Github is on the third 
 
 
 
-# Part 2: Working with the response dictionary
+# # Part 2: Working with the response dictionary
+# r = requests.get(url, headers=headers)
+# response_dict = r.json()
+#
+# print(f"Total repositories: {response_dict['total_count']}") # prints the total amount of repositories under 'total_count'
+#
+# # Explore the information about the repositories
+# repo_dicts = response_dict['items'] # Takes all repositories from the dictionary titled 'items' and stores them in a variable
+# print(f"Repositories returned: {len(repo_dicts)}")
+#
+# # Examine the first repository
+# repo_dict = repo_dicts[0] # takes the first repository and prints out all the keys
+# print(f"\nKeys: {len(repo_dict)}")
+# for key in sorted(repo_dict.keys()): # for loop that returns the name of every key in the first repository
+#     print(key)
+
+
+
+
+# Part 3: Start returning the values associated with some of the keys
 r = requests.get(url, headers=headers)
 response_dict = r.json()
 
+print(f"Status code: {r.status_code}")
 print(f"Total repositories: {response_dict['total_count']}")
 
-# Explore the information about the repositories
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Examine the first repository
 repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+
+print("\nSelected information from the first repository: ")
+
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
