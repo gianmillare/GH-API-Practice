b4d02dfb83d41031824d9bac6f5d0e9b35809ce5
gianmillare <gianmillaregsm@gmail.com>
2020-03-22 14:49:06 -0700

    returned all information from key value pairing from all repositories

--- python_repos.py
+++ python_repos.py
@@ -42,24 +42,45 @@ headers = {'Accept': 'application/vnd.github.v3+json'} # Github is on the third 
 
 
 
-# Part 3: Start returning the values associated with some of the keys
+# # Part 3: Start returning the values associated with some of the keys
+# r = requests.get(url, headers=headers)
+# response_dict = r.json()
+#
+# print(f"Status code: {r.status_code}")
+# print(f"Total repositories: {response_dict['total_count']}")
+#
+# repo_dicts = response_dict['items']
+# print(f"Repositories returned: {len(repo_dicts)}")
+#
+# repo_dict = repo_dicts[0]
+#
+# print("\nSelected information from the first repository: ")
+#
+# print(f"Name: {repo_dict['name']}")
+# print(f"Owner: {repo_dict['owner']['login']}")
+# print(f"Stars: {repo_dict['stargazers_count']}")
+# print(f"Repository: {repo_dict['html_url']}")
+# print(f"Created: {repo_dict['created_at']}")
+# print(f"Updated: {repo_dict['updated_at']}")
+# print(f"Description: {repo_dict['description']}")
+
+
+
+# Part 4: Summarizing the top repositories
 r = requests.get(url, headers=headers)
 response_dict = r.json()
 
-print(f"Status code: {r.status_code}")
-print(f"Total repositories: {response_dict['total_count']}")
+print(f"Status Code: {r.status_code}")
+print(f"Total Repositories: {response_dict['total_count']}")
 
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+print(f"Returned Repositories: {len(repo_dicts)}")
 
-repo_dict = repo_dicts[0]
+print("\nSelected Information about each repository: ")
+for repo_dict in repo_dicts: # For Loop that loops through all keys in the repository and returns specific information
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
 
-print("\nSelected information from the first repository: ")
-
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
