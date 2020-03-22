6a0b8da8e6775209df298354db345fac391543cb
gianmillare <gianmillaregsm@gmail.com>
2020-03-22 15:35:54 -0700

    refined the plotly chart by altering the titles and the graph color

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -6,32 +6,74 @@ from plotly import offline
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 
-# Part 5: Visualizing repositories using plotly
-r = requests.get(url, headers=headers)
-print(f"Status code: {r.status_code}")
+# # Part 5: Visualizing repositories using plotly
+# r = requests.get(url, headers=headers)
+# print(f"Status code: {r.status_code}")
+#
+# # Process the results
+# response_dict = r.json()
+# repo_dicts = response_dict['items']
+#
+# repo_names, stars = [], []
+# for repo_dict in repo_dicts:
+#     repo_names.append(repo_dict['name'])
+#     stars.append(repo_dict['stargazers_count'])
+#
+# # Visualize the data
+# data = [{
+#     'type': 'bar',
+#     'x': repo_names,
+#     'y': stars,
+# }]
+#
+# my_layout = {
+#     'title': 'Most Starred Python Projects on GitHub',
+#     'xaxis': {'title': 'Repository'},
+#     'yaxis': {'title': 'Stars'},
+# }
+#
+# fig = {'data': data, 'layout': my_layout}
+# offline.plot(fig, filename="plots/python_repos.html")
 
-# Process the results
+
+
+# Part 6: Refining the plotly chart
+r = requests.get(url, headers=headers)
+print(f"Status Code: {r.status_code}")
+
 response_dict = r.json()
 repo_dicts = response_dict['items']
 
 repo_names, stars = [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    stars.append((repo_dict['stargazers_count']))
 
-# Visualize the data
 data = [{
     'type': 'bar',
     'x': repo_names,
     'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
 }]
 
 my_layout = {
     'title': 'Most Starred Python Projects on GitHub',
-    'xaxis': {'title': 'Repository'},
-    'yaxis': {'title': 'Stars'},
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14}
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
 }
 
 fig = {'data': data, 'layout': my_layout}
-offline.plot(fig, filename="plots/python_repos.html")
-
+offline.plot(fig, filename='plots/python_repos2.html')
